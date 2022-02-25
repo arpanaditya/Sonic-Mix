69f3ec3d8ec62af85ceaa9ca2f1ac4366adfb6ae
Aditya Arpan Sahoo <adityaarpansahoo@gmail.com>
2022-02-25 11:41:14 +0530

    added song list

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Music List
+
+let music = [
+    {
+        title: 'Paris',
+        artist: 'The Chainsmokers',
+        song_path: 'music/paris.mp3',
+        image_path: 'img/paris.jpg'
+    },
+    {
+        title: 'Look What You Made Me Do',
+        artist: 'Taylor Swift',
+        song_path: 'music/look_what_you_made_me_do.mp3',
+        image_path: 'img/look_what_you_made_me_do.jpg'
+    },
+    {
+        title: 'High',
+        artist: 'The Chainsmokers',
+        song_path: 'music/High.mp3',
+        image_path: 'img/high.jpg'
+    },
+    {
+        title: 'Stay',
+        artist: 'The kid LAROI & Justin Bieber',
+        song_path: 'music/stay.mp3',
+        image_path: 'img/stay.jpg'
+    },
+    {
+        title: 'Something Just Like This',
+        artist: 'The Chainsmokers',
+        song_path: 'music/something_just_like_this.mp3',
+        image_path: 'img/something_just_like_this.jpg'
+    }
+]
