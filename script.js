9c9a6fb51b80216fa6fa7edf62633393ac76bc78
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-03-08 12:47:56 +0530

    add autoplay feature when one songs finishes (#7)

--- script.js
+++ script.js
@@ -62,7 +62,8 @@ window.onload = () => {
       image_path: 'img/sunflower.jpg'
     }
   ];
-
+   // Autoplay next song when current song finishes
+  player.addEventListener("ended", changeMusic);
   play_btn.addEventListener('click', togglePlay);
   next_btn.addEventListener('click', () => changeMusic());
   previous_btn.addEventListener('click', () => changeMusic(false));
